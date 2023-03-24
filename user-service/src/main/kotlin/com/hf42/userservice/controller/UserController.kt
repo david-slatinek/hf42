bffaba012ed06816c468c249524adaa5b65df227
david-slatinek <david.slatinek5@gmail.com>
2023-03-24 21:17:28 +0100

    Delete swagger

--- user-service/src/main/kotlin/com/hf42/userservice/controller/UserController.kt
+++ user-service/src/main/kotlin/com/hf42/userservice/controller/UserController.kt
@@ -3,10 +3,6 @@ package com.hf42.userservice.controller
 import com.hf42.userservice.model.Login
 import com.hf42.userservice.model.User
 import com.hf42.userservice.repository.UserRepository
-import io.swagger.v3.oas.annotations.Operation
-import io.swagger.v3.oas.annotations.media.Content
-import io.swagger.v3.oas.annotations.responses.ApiResponse
-import io.swagger.v3.oas.annotations.responses.ApiResponses
 import jakarta.validation.Valid
 import org.mindrot.jbcrypt.BCrypt
 import org.springframework.http.HttpStatus
@@ -22,25 +18,6 @@ import org.springframework.web.bind.annotation.*
 )
 
 class UserController(private val userRepository: UserRepository) {
-    @Operation(summary = "Register a new user")
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "201",
-                description = "Created",
-                content = [Content(
-                    mediaType = "application/json",
-                    schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Map::class)
-                )]
-            ),
-            ApiResponse(
-                responseCode = "400", description = "Bad Request", content = [Content(
-                    mediaType = "application/json",
-                    schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Map::class)
-                )]
-            ),
-        ]
-    )
     @PostMapping("/register")
     fun register(@Valid @RequestBody user: User): ResponseEntity<Map<String, String>> {
         if (userRepository.findByEmail(user.email) != null) {
@@ -52,23 +29,6 @@ class UserController(private val userRepository: UserRepository) {
         return ResponseEntity(mapOf("id" to obj.id.toString()), HttpStatus.CREATED);
     }
 
-    @Operation(summary = "Login a user")
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "200", description = "OK", content = [Content(
-                    mediaType = "application/json",
-                    schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Map::class)
-                )]
-            ),
-            ApiResponse(
-                responseCode = "400", description = "Bad Request", content = [Content(
-                    mediaType = "application/json",
-                    schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Map::class)
-                )]
-            ),
-        ]
-    )
     @PostMapping("/login")
     fun login(@Valid @RequestBody login: Login): ResponseEntity<Map<String, String>> {
         val user = userRepository.findByEmail(login.email)
@@ -81,17 +41,6 @@ class UserController(private val userRepository: UserRepository) {
         return ResponseEntity(mapOf("message" to "login successful"), HttpStatus.OK);
     }
 
-    @Operation(summary = "Delete a user by id")
-    @ApiResponses(
-        value = [
-            ApiResponse(
-                responseCode = "200", description = "OK", content = [Content(
-                    mediaType = "application/json",
-                    schema = io.swagger.v3.oas.annotations.media.Schema(implementation = Map::class)
-                )]
-            ),
-        ]
-    )
     @DeleteMapping("/{userId}")
     fun delete(@PathVariable userId: String): ResponseEntity<Map<String, String>> {
         userRepository.deleteById(userId)
