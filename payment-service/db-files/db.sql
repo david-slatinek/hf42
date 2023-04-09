6796e13f4341a4c24397fec2ea4024b9bf269073
david-slatinek <david.slatinek5@gmail.com>
2023-04-09 08:57:22 +0200

    Add db-files

---
+++ payment-service/db-files/db.sql
@@ -0,0 +1,15 @@
+USE payment_db;
+
+SET FOREIGN_KEY_CHECKS = 0;
+DROP TABLE IF EXISTS payment;
+SET FOREIGN_KEY_CHECKS = 1;
+
+CREATE TABLE payment
+(
+    id_payment INT            NOT NULL AUTO_INCREMENT,
+    order_id   VARCHAR(255)   NOT NULL,
+    user_id    VARCHAR(255)   NOT NULL,
+    amount     DECIMAL(10, 2) NOT NULL,
+    order_date DATETIME       NOT NULL,
+    PRIMARY KEY (id_payment)
+);
