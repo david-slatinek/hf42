6937325a644929f05657fbe7432d40f9a13de10b
david-slatinek <david.slatinek5@gmail.com>
2023-05-01 10:11:05 +0200

    Update init

--- website/display/src/App.tsx
+++ website/display/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
 
 import "./index.scss";
 import Display from "./Display";
+import {createRoot} from "react-dom/client";
 
 const App = () => (
-    <Display/>
+    <div>
+        <Display/>
+    </div>
 );
-ReactDOM.render(<App/>, document.getElementById("app"));
+
+const rootElement = document.getElementById("app")!;
+const root = createRoot(rootElement);
+root.render(<App/>);
+
